946681e4befe6254a654889a76dfdc0fe71dc9fe
Abdul Moiz <74152199+Abdul-Moiz13@users.noreply.github.com>
2022-08-13 21:42:28 +0500

    Additional files

---
+++ fb.js
@@ -0,0 +1,3 @@
+alert("Welcome to the new version of facebook to enjoy the version kindly verify you're not a robot")
+var verify = prompt("“What is your oldest cousin's first name?")
+alert("Enjoy Now")
